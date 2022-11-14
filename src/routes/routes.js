b09677377119705412622db8e40173de5ea9ab50
bess10 <salgadobessy10@gmail.com>
2022-11-14 16:16:42 -0600

    añadiendo fichero js donde estan las rutas de la app

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,8 +2,10 @@ import {Router} from 'express';
 const router = Router();
 import {scraping} from '../controllers/controllers.js';
 
+//se indica la ruta y la función a ejecutar
 router.get('/productos', scraping);
 
+//se exporta el router
 export default router;
 
 
