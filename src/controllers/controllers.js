68a9d94f1d09ddad095aa250b9f39e4375eeee2d
bess10 <salgadobessy10@gmail.com>
2022-11-14 16:18:02 -0600

    añadiendo fichero js aqui va la logica de la app

--- src/controllers/controllers.js
+++ src/controllers/controllers.js
@@ -1,9 +1,10 @@
-//import { response } from 'express';
+//importamos los modulos necesarios
 import  cheerio from 'cheerio';
 import axios from 'axios';
-
+//guardamos en una constante la url
 const URL = 'https://webscraper.io/test-sites/e-commerce/allinone/computers';
 
+//exportamos la función que se utilizara en la ruta productos 
 export const scraping = (req, res) => {
     axios(URL)
     .then((response) => {
@@ -11,6 +12,7 @@ export const scraping = (req, res) => {
         const $ = cheerio.load(html);
         const producto = [];
 
+        //capturamos los elementos necesarios en el div con clase caption
         $('div.caption', html).each(function () {
             const nombre = $(this).find('a.title').attr('title');
             const precio = $(this).find('h4.price').text();
@@ -22,6 +24,7 @@ export const scraping = (req, res) => {
                 descripcion
             });
         });
+        //se manda como respuesta el array de objetos en formato json
         res.json(producto);
     })
     .catch((err) => console.log(err));
