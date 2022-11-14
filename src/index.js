786a41155532edf606d6ed9f2bf55f7cd39c3ea5
bess10 <salgadobessy10@gmail.com>
2022-11-14 16:18:58 -0600

    añadiendo fichero js aqui se arranca el servidor

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
+//importamos los modulos necesarios
 import express from 'express';
 import router  from './routes/routes.js';
 import cors from 'cors';
+
 const app = express();
 const PUERTO = 4000;
 
 app.use(router);
 app.use(cors());
+
+//asignamos el puerto 
 app.listen(PUERTO, console.log(`Escuchando por el puerto ${PUERTO}`));
